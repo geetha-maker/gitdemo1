93d6b9bb642f932915b48d6175c50307f2a6c500
geetha-maker <anjaligeetharavi2005@gmail.com>
2023-05-22 13:10:39 +0530

    second commit

--- add.java
+++ add.java
@@ -2,6 +2,9 @@ class Add
 {
     public static void main(String [] args)
     {
-        System.out.println("Hello");
+        int a=20;
+        int b=20;
+        int sum=a+b;
+        System.out.println("sum "+sum);
     }
 }
